a5c4029e03617b6338daca90be2d378d6ac3d4c9
itatipulgar <85032891+itatipulgar@users.noreply.github.com>
2021-10-26 23:17:10 -0300

    cambio

--- Presentacion/ProductosListado.cs
+++ Presentacion/ProductosListado.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using Capa_Entidad;
 using Capa_Negocio;
@@ -14,10 +18,11 @@ namespace Presentacion
         {
             InitializeComponent();
         }
-        public String Tipo = "";
-        Producto produ = new Producto();
 
+        private void lbl_RazonSocial_Click(object sender, EventArgs e)
+        {
 
+        }
 
         private void btn_ModifProv_Click(object sender, EventArgs e)
         {
@@ -25,74 +30,45 @@ namespace Presentacion
             prod.Show();
         }
 
-
-
-        private void dgv_ListProductos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void btn_BuscarProducto_Click(object sender, EventArgs e)
         {
 
-        }
-
-        public void btn_BuscarProducto_Click(object sender, EventArgs e)
-        {
+          ProductoNegocio prodNegocio = new ProductoNegocio();
 
 
-            //DataSet ds = new DataSet();
-            //DataTable dt = new DataTable();
-            //var ProdNego = new ProductoNegocio();
-            //var prod = new Producto();
-            //dt = ProdNego.ConsultarProductoTipo(prod);
-            var dt = new DataTable();
-            produ.prod_Tipo = cBox_TipoProd.Text;
-            var ProdNego = new ProductoNegocio();
-            dt = ProdNego.ConsultarProductoTipo(produ);
-            
-            if (dt.Rows.Count != 0)
+            if (cBox_TipoProd.Text != "")
             {
+             //   producto.prod_Tipo = cBox_TipoProd.SelectedItem.ToString() ;
 
-              
-
-
-
-                dgv_ListProductos.DataSource = dt; //ds.Tables[0];
-
+                //// producto.prod_Tipo = cBox_TipoProd.Text;
+                //DataTable dt = new DataTable();
+                //dt = prodNegocio.ConsultarProdXTipo(producto);
+                //dgv_ListProductos.DataSource = dt;
             }
             else
             {
-                MessageBox.Show("No hay datos registados.");
+                MessageBox.Show("No hay disponibilidad del producto seleccionado, seleccione otro tipo");
+
+
             }
 
-            //if (dt.Rows.Count != 0)
-            //{
-            //    dgv_ListProductos.DataSource = dt; //ds.Tables[0];
-
-            //}
-            //else
-            //{
-            //    MessageBox.Show("no hay productos disponibles para temporada, tipo y subtipo seleccionado");
-            //}
-        }
-
-
-
-
-
-
-        private void btn_CerrarUsuAlta_Click(object sender, EventArgs e)
-        {
-            this.Close();
 
         }
 
         private void cBox_TipoProd_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Tipo = cBox_TipoProd.Text;
-
+          
 
         }
 
         private void ProductosListado_Load(object sender, EventArgs e)
         {
-           
+             cBox_TipoProd.SelectedIndex = 0;
         }
     }
 }
+
+
+
+
+
